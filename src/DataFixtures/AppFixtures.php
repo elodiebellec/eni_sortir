9a02f39e3701e2a1d89af85e2e3255535565c324
Dmoulart <dorian.moulart@gmail.com>
2021-05-31 16:50:38 +0200

    Add register states function

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,52 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\State;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker;
 
 class AppFixtures extends Fixture
 {
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $generator = Faker\Factory::create('fr_FR');
+
+        $this->registerStates($manager);
+
+        for($i=0;$i<20;$i++){
+
+        }
 
         $manager->flush();
+    }
+
+    private function registerStates(ObjectManager $manager){
+        /*DEFINE STATES*/
+        $created = new State;
+        $created->setLabel("Créée");
+
+        $opened = new State;
+        $opened->setLabel("Ouverte");
+
+        $fenced = new State;
+        $fenced->setLabel("Clôturée");
+
+        $inProgress = new State;
+        $inProgress->setLabel("Activité en cours");
+
+        $finished  = new State;
+        $finished->setLabel("Activité passée");
+
+        $canceled  = new State;
+        $canceled ->setLabel("Activité annulée");
+
+        /*PERSIST STATES*/
+        $manager->persist($created);
+        $manager->persist($fenced);
+        $manager->persist($inProgress);
+        $manager->persist($finished);
+        $manager->persist($canceled);
     }
 }
