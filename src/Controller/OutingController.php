5844cf09ffb2201ec904b4767b6254f4b798fe7b
Dmoulart <dorian.moulart@gmail.com>
2021-06-01 22:50:44 +0200

    Add participant outing registration

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
 use App\Repository\OutingRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -26,5 +28,35 @@ class OutingController extends AbstractController
         return $this->render('outing/list.html.twig', ["outings"=>$outings, "currentPage"=> $page, "maxPage"=>$maxPage
 
         ]);
+    }
+
+    /**
+     * @IsGranted("ROLE_USER")
+     * @Route("outing/addParticipant/{id}", name="outing_registerto", requirements={"page"="\d+"})
+     */
+    public function addParticipant(int $id,
+                                   OutingRepository  $outingRepository,
+                                   EntityManagerInterface $entityManager){
+
+        $outing = $outingRepository->find($id);
+
+        /**
+         * @var $user Participant
+         */
+        $user = $this->getUser();
+
+        if($user->getOutings()->contains($outing)){
+            $this->addFlash('operation denied','Vous êtes déjà inscrit à cet évènement.');
+            return $this->redirectToRoute('outing');
+        }
+        if(strcmp($outing->getPlanner()->getPseudo(), $user->getPseudo()) === 0){
+            $this->addFlash('operation denied',"Vous organisez cet évènement.");
+            return $this->redirectToRoute('outing');
+        }
+
+        $entityManager->persist($outing->addParticipant($user));
+        $entityManager->flush();
+
+        return $this->redirectToRoute('outing');
     }
 }
