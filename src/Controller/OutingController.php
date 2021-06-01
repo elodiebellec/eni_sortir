431dcfb837cdd1a60fc8642f8b8b27892f2a2c0a
mamibetina <none>
2021-06-01 11:46:02 +0200

    create method list, create twig file

--- src/Controller/OutingController.php
+++ src/Controller/OutingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OutingRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,17 @@ class OutingController extends AbstractController
     {
         return $this->render('outing/index.html.twig', [
             'controller_name' => 'OutingController',
+        ]);
+    }
+
+    public function  list (OutingRepository  $outingRepository): Response
+    {
+
+
+        $outings =  $outingRepository->findAll();
+
+        return $this->render('outing/list.html.twig', ["outings"=>$outings
+
         ]);
     }
 }
