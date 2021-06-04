bbc5d1af4302a1d98177e2ba4d3bfedea618a2d9
elodie.b <elodie.bellec@gmail.com>
2021-06-04 15:21:19 +0200

    Delete unused import

--- src/Form/OutingType.php
+++ src/Form/OutingType.php
@@ -3,20 +3,16 @@
 namespace App\Form;
 
 
-use App\Entity\City;
 use App\Entity\Location;
 use App\Entity\Outing;
 
 use App\Repository\CityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
